7de65233cd5e353ff228b1c616a6dae84f609185
Adrian Sampson <adrian@radbox.org>
2023-12-18 17:19:36 -0500

    DummyIO: Check for double "installs"

--- beets/test/_common.py
+++ beets/test/_common.py
@@ -192,6 +192,7 @@ class DummyIO:
         return self.stdin.reads
 
     def install(self):
+        assert not self.installed, "DummyIO already installed"
         self.installed = True
 
         self.orig_stdin = sys.stdin
